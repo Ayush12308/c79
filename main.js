747f7e0085e8ec28f856455c44ece20860aca89b
Ayush12308 <117008435+Ayush12308@users.noreply.github.com>
2023-04-12 14:51:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var name_of_the_student= [];
+
+function submit(){
+    var name1 = document.getElementById("name_of_student1").value;
+    var name2 = document.getElementById("name_of_student2").value;
+    var name3 = document.getElementById("name_of_student3").value;
+    var name4 = document.getElementById("name_of_student4").value;
+
+    name_of_the_student.push(name1);
+    name_of_the_student.push(name2);
+    name_of_the_student.push(name3);
+    name_of_the_student.push(name4);
+
+    console.log(name_of_the_student);
+
+    document.getElementById("display_name").innerHTML=name_of_the_student;
+    document.getElementById("submit_b").style.display="none";
+    document.getElementById("sort_b").style.display="inline-block";
+}
+
+function sorting()
+{
+
+    name_of_the_student.sort();
+    console.log(name_of_the_student);
+    document.getElementById("display_name").innerHTML=name_of_the_student;
+}
